a3f657abe2a7d1cca63bbc9c98e93013a2b3f163
lewis <lingyidx@gmail.com>
2021-02-24 00:16:19 +0800

    feat: 添加处理器

--- src/main/java/com/github/lewisbyte/annotation/GsnSeq.java
+++ src/main/java/com/github/lewisbyte/annotation/GsnSeq.java
@@ -1,7 +1,12 @@
 package com.github.lewisbyte.annotation;
 
+import java.lang.annotation.*;
+
 /**
  * 生成流水注解
  */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface GsnSeq {
 }
---
+++ src/main/java/com/github/lewisbyte/resolvers/GsnSeqResolvers.java
@@ -0,0 +1,11 @@
+package com.github.lewisbyte.resolvers;
+
+/**
+ * @program: transction-sequence-spring-boot-starter
+ * @description: 流水注解处理器
+ * @author: lewis
+ * @create: 2021-02-24 00:15
+ *
+ */
+public class GsnSeqResolvers {
+}
